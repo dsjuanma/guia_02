c3d335a07f98da9e6598e9152ecec8b3fb2a5a7d
dsjuanma <juanmanuel_dasilva@hotmail.com>
2021-03-25 19:56:08 -0300

    en proceso

--- src/guia_02/Libro.java
+++ src/guia_02/Libro.java
@@ -1,4 +1,147 @@
 package guia_02;
 
+
+/****************************************************************
+ *
+ * 1. Es necesario modelar el objeto de tipo Libro con las siguientes caracteristicas, titulo,
+ *    precio, stock y Autor, este último posee las características de nombre, apellido,
+ *    email y genero ( ‘M’ o ‘F’ ). Para este ejercicio vamos a asumir que un libro tiene un
+ *    único autor. Ejecute las siguientes instrucciones:
+ *    a. Inicialice un objeto de tipo Autor, “Joshua”, “Bloch”, “joshua@email.com”, ’M’.
+ *    b. Imprima por pantalla al autor previamente instanciado.
+ *    c. Inicialice el libro “Effective Java” del Autor “Joshua Bloch” que cuesta 450
+ *    pesos con una cantidad de 150 copias.
+ *    d. Imprima por pantalla el libro instanciado.
+ *    e. Modifique el precio del libro “Effective Java” a 500 pesos y aumente la
+ *    cantidad en 50 copias.
+ *    f. Imprima por pantalla los atributos del Autor Joshua, accediendo desde el
+ *    Libro “Effective Java”.
+ *    g. Agregue un método a la clase Libro que posibilite imprimir en pantalla el
+ *    siguiente mensaje:
+ *    “El libro, {título} de {nombre del autor}. Se vende a {precio} pesos.”
+ *     h. Modificar la clase Libro, para que acepte más de 1 Autor. Y realizar los
+ *     cambios necesarios en el método del punto g, para imprimir un nuevo
+ *     mensaje que liste los autores que contribuyeron a ese libro.
+ *
+ ***************************************************************/
+
+
 public class Libro {
+
+    //Atributos
+    private String titulo = "Effective Java";
+    private double precio = 450;
+    private int stock = 150;
+    private String nombre = "Joshua";
+    private String apellido = "Bloch";
+    private String email = "joshua@email.com";
+    private char genero = 'M';
+
+    //Constructor
+    public Libro() {
+
+    }
+
+    public Libro(String titulo, double precio, int stock, String nombre, String apellido, String email, char genero) {
+        this.titulo = titulo;
+        this.precio = precio;
+        this.stock = stock;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.genero = genero;
+    }
+
+    //Getter y Setter
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    /*
+    b. Imprima por pantalla al autor previamente instanciado.
+     */
+    public String imprimeAutor() {
+        return "Nombre:" + this.nombre + " | " + " Apellido:" + this.apellido + " | " + " Email:" + this.email + " | " + " Genero:" + this.genero;
+    }
+   /*
+   e. Modifique el precio del libro “Effective Java” a 500 pesos y aumente la
+   *  cantidad en 50 copias.
+   */
+    public String cambioTitulo(String nombreLibro){
+        return this.titulo = nombreLibro;
+    }
+    /*
+     *  f. Imprima por pantalla los atributos del Autor Joshua, accediendo desde el
+     *  Libro “Effective Java”.
+     */
+    public String buscaAutoreImprime(String buscaAutor){
+        if(this.nombre == buscaAutor){
+            return "Lirbo:" + this.titulo + " | " + " Precio:" + this.precio + " | " + " Stock:" + this.stock ;
+        }else{
+            System.out.println("Autor no encontrado...");
+        }
+
+    }
+    /* Metodo que se habia creado para llamar en el metodo de Busca autor e Imprime, se decidio mostrar todo en un mismo metodo
+
+    public String imprimeLibro() {
+        return "Lirbo:" + this.titulo + " | " + " Precio:" + this.precio + " | " + " Stock:" + this.stock ;
+    }
+
+     */
+
+
+
 }
--- src/guia_02/Main.java
+++ src/guia_02/Main.java
@@ -1,8 +1,97 @@
 package guia_02;
 
+import javax.swing.filechooser.FileSystemView;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class Main {
 
+
+    public static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+        int selected;
+
+        do {
+            selected = menu();
+            switch (selected) {
+                case 1:
+                    Libro autor = new Libro();
+
+                    System.out.println("Ingrese el nuevo nombre del libro ");
+                    String nombre = scan.next();
+                    autor.setTitulo(nombre);
+                    System.out.println(autor.imprimeAutor());
+                    Pausa.pausar();
+                    limpiarPantalla();
+                    break;
+                case 2:
+
+
+                    Pausa.pausar();
+                    limpiarPantalla();
+                    break;
+                case 3:
+
+                    Pausa.pausar();
+                    limpiarPantalla();
+
+                case 0:
+                    System.out.println("Programa finalizado.");
+                    break;
+                default:
+                    limpiarPantalla();
+                    System.out.println("¡¡¡Ejercicio Inexistente.!!!");
+                    Pausa.pausar();
+                    limpiarPantalla();
+                    break;
+            }
+        } while (selected != 0);
+    }
+
+
+    public static int menu() { // Menu
+        System.out.println("\n\t____________________________________________________________________");
+        System.out.println("\n");
+        System.out.println("\t    GUIA 00: INTRODUCCION A JAVA.");
+        System.out.println("\n\t____________________________________________________________________");
+        System.out.println("\n");
+        System.out.println("\t    MENU");
+        System.out.println("\n\t____________________________________________________________________");
+        System.out.println("\n");
+        System.out.println("\t   [1].Ejercicio 01.");
+        System.out.println("\t   [2].Ejercicio 02.");
+        System.out.println("\t   [3].Ejercicio 03.");
+        System.out.println("\t   [4].Ejercicio 04.");
+        System.out.println("\t   [5].Ejercicio 05.");
+
+        System.out.println("\n\t____________________________________________________________________");
+        System.out.println("\n");
+        System.out.println("\t   [0].Salir del programa.");
+        System.out.println("\n\t____________________________________________________________________");
+        System.out.println("\n\n\t  Elija una opcion: ");
+        System.out.println("\n");
+        int input = scan.nextInt();
+        return input;
+    }
+
+
+    public static void limpiarPantalla() {         // se dejan 60 espacios creando una especie de cls de la consola
+        for (int i = 0; i < 2; i++) {
+            System.out.println("\n");
+        }
+    }
+
+
+    public static class Pausa {
+        public static void pausar() {
+            System.out.println("\n");
+            System.out.println(" ----------------------------------------");
+            System.out.println("|Presionar la tecla ENTER para continuar.|");
+            System.out.println(" ----------------------------------------");
+            Scanner aceptar = new Scanner(System.in);
+            System.out.println("\n");
+            aceptar.nextLine(); // queda esperando que precionemos la tecla enter para continuar
+        }
     }
 }
